62df3391062278da07563b88f942ea5c77edde58
zzsxd <nikitacapkov06@gmail.com>
2024-02-29 01:48:37 +0800

    PDF in python VER 2.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fpdf import FPDF
-from PIL import Image
 
 # Создаем PDF файл с дизайном
 pdf = FPDF()
@@ -13,23 +12,22 @@ pdf.image('FirstPage.png', x=0, y=0, w=210, h=297)
 pdf.add_page()
 pdf.image('SecondPage.png', x=0, y=0, w=210, h=297)
 pdf.set_xy(0, 100)
-pdf.set_font('Arial', size=28)
-company_name = 'IWonIWonIWon'
-pdf.cell(350, -100, company_name, 0, 1, 'C')
+pdf.set_text_color(255, 255, 255)
+pdf.set_font('Arial', size=28, style='B')
+company_name = 'Your Company Name'
+pdf.cell(220, -100, company_name, 0, 1, 'C')
+pdf.set_text_color(0, 0, 0)
 pdf.set_font('Arial', size=15)
-
 # Добавляем ответы бота на 10 вопросов
 pdf.set_xy(0, 100)
-text = 'ebaniy sir'
-lines = pdf.multi_cell(w=0, h=10, txt=text)
+text = ['First Name', 'Last Name', 'Email', 'Phone Number', 'Address', 'City', 'State', 'Country']
+lines = pdf.multi_cell(w=0, h=10, txt=text[0])
 
-# # Добавляем третью страницу в PDF файл
-# pdf.add_page()
-# pdf.image('ThirdPage.png', x=0, y=0, w=210, h=297)
-# pdf.set_xy(0, 100)
-# pdf.set_font('Arial', size=28)
-# pdf.cell(210, 0, company_name, 0, 1, 'C')
-# pdf.set_font('Arial', size=15)
+# # Добавляем третью и тд страницу в PDF файл
+for i in range(1, len(text)):
+    pdf.add_page()
+    pdf.image('ThirdPage.png', x=0, y=0, w=210, h=297)
+    pdf.multi_cell(w=0, h=10, txt=text[i])
 
 # # Добавляем текст-рекомендацию
 # pdf.multi_cell(w=0, h=10, txt=text1)
